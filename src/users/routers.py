f374eed839ae5e82ca9af4119e5d5eafb4077398
gildasvictorin <gildasvictorin@gmail.com>
2026-08-01 20:39:06 +0200

    Create-update-Modify User

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,9 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from src.users.models import User
-from src.users.schemas import UserResponse
+from src.users.schemas import UserResponse, UserCreate, UserUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
 
 from src.databese import get_async_session
 
@@ -21,3 +22,51 @@ async def get_users(session: AsyncSession = Depends(get_async_session)):
     return result
 #after this import in main.py(from src.users.routers import router as users_router)
 # and app.include_router(users_router)
+@router.post("/", response_model=UserResponse)
+async def create_user(payload: UserCreate, session: AsyncSession = Depends(get_async_session)):
+    new_user = User(
+        username=payload.username,
+        password=payload.password,
+        email=payload.email,
+        is_admin=payload.is_admin
+    )
+    session.add(new_user)
+    try:
+        await session.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Username or email already in user")
+    return new_user
+
+#How to get, modify and delete USER
+@router.get("/{user_id}", response_model=UserResponse)
+async def get_users(user_id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(User).where(User.id == user_id)
+    query_result = await session.scalars(query)
+    result = query_result.first()
+    if not result:
+        raise HTTPException(status_code=404, detail="User not found")
+    return result
+
+#modify user and go to schemas.py
+##to modify user keeping all models created adding new class
+
+@router.patch("/{user_id}", response_model=UserResponse)
+async def update_user(user_id: int, payload: UserUpdate, session: AsyncSession = Depends(get_async_session)):
+    query = select(User).where(User.id == user_id)
+    query_result = await session.scalars(query)
+    result = query_result.first()
+    if not result:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    for field, value in payload.model_dump().items():
+        if value is not None:
+            setattr(result, field, value)
+
+    try:
+        await session.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Username or email already in user")
+    return result
+
+
+
--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,10 +1,28 @@
+from typing import Optional
 from src.schemas import CustomBase
-
+from datetime import datetime
 from pydantic import PositiveInt, Field, EmailStr
 
 
 class UserResponse(CustomBase):
     id: PositiveInt
-    username: str = Field(..., min_length=5, max_length=30)
+    username: str = Field(..., min_length=5, max_length=50, examples=["ciccio"])
     email: EmailStr
-    is_admin: bool 
+    is_admin: bool
+    created_id: datetime
+
+
+#how to create user and create it inside router with decorator
+
+class UserCreate(CustomBase):
+    username: str = Field(..., min_length=5, max_length=50, examples=["ciccio"])
+    email: EmailStr
+    password: str = Field(..., min_length=5, max_length=30, examples=["ciccio123"])
+    is_admin: bool = Field(False)
+
+#to modify user keeping all models create and import optional
+class UserUpdate(CustomBase):
+    username: Optional[str] = Field(None, min_length=5, max_length=50, examples=["ciccio"])
+    email: Optional[EmailStr] = Field(None)
+    password: Optional[str] = Field(None, min_length=5, max_length=30, examples=["ciccio123"])
+    is_admin: Optional[bool] = Field(None)
